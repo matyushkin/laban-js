32444d65073c07debd8db0dcb288a7bdbf27f719
Leo Matyushkin <leva.matyushkin@gmail.com>
2020-04-10 12:10:23 +0300

    Get all elements from inputs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-// - get all infp from inputs and buttons
+/* 
+TODO:
+- NOW: Add info to wiki from Miroslav Project, add video
+- NOW: Start mode: beginner is checked
+ - Make placeholders for inputs from javascript
+ - Make function that will generate text for placeholders of inputs
+ - Get Audio from Miroslav repo
+ - Make function to play audiofiles sequence
+ - Add instructions
+ */
 
 import "./styles.css";
 
@@ -6,7 +15,10 @@ document.getElementById("app").innerHTML = `
 `;
 
 function get_data() {
-  alert(document.getElementById("combination").value);
+  var combination = document.getElementById("combination").value;
+  var time_rest = document.getElementById("time_rest").value;
+  var repetitions = document.getElementById("repetitions").value;
+  alert(combination + time_rest + repetitions);
 }
 
 document.getElementById("btn_play").onclick = get_data;
