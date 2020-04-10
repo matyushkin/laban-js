9c15210d60a8bbee2b44e298b84bbea919d9018b
matyushkin <leva.matyushkin@gmail.com>
2020-04-10 23:47:09 +0300

    New Design stage

--- src/index.js
+++ src/index.js
@@ -2,52 +2,44 @@
 - If user changes level by radio button it must change dinamically inputs placeholders 
  */
 
+let level = "";
+
+let radios = document.getElementsByName("level");
+
+for (let i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+    level = radios[i].value;
+    break;
+  }
+}
+
 const range = (start, end, length = end - start + 1) =>
   Array.from({ length }, (_, i) => start + i);
 
-const level = get_level();
-const COMBINATION_ARR_DEFAULT = generate_combination(level);
-const COMBINATION_STR_DEFAULT = COMBINATION_ARR_DEFAULT.join("");
-const TIME_REST_DEFAULT = generate_time_rest(level);
-const ITERATIONS_DEFAULT = generate_iterations(level);
-
-function fill_placeholders(
-  combination_str = COMBINATION_STR_DEFAULT,
-  time_rest = TIME_REST_DEFAULT,
-  iterations = ITERATIONS_DEFAULT
-) {
-  //  fill input forms with default or generated values
-  document.getElementsByName("combination")[0].placeholder = combination_str;
-  document.getElementsByName("time_rest")[0].placeholder = time_rest;
-  document.getElementsByName("iterations")[0].placeholder = iterations;
-}
-
-function get_level() {
-  let level_flag = document.getElementsByName("level")[0].value;
-  return level_flag;
-}
-
-function get_data() {
-  let combination_str = document.getElementsByName("combination")[0].value;
-  let time_rest = document.getElementsByName("time_rest")[0].value;
-  let iterations = document.getElementsByName("iterations")[0].value;
-
-  // If user doesn't fill some input, use deafault constants
-  if (combination_str === "") {
-    combination_str = COMBINATION_STR_DEFAULT;
-  }
-  if (time_rest === "") {
-    time_rest = TIME_REST_DEFAULT;
-  }
-  if (iterations === "") {
-    iterations = ITERATIONS_DEFAULT;
-  }
-  alert(combination_str + time_rest + iterations);
-}
-
-// functions for data random generation
 function randomChoice(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generate_combination(level) {
+  let min = 3;
+  let max = 5;
+
+  if (level === "Middle") {
+    min = 5;
+    max = 7;
+  }
+  if (level === "Profi") {
+    min = 7;
+    max = 10;
+  }
+
+  let comb_length = randomChoice(range(min, max));
+
+  let arr = [];
+  for (var i = 0; i < comb_length; i++) {
+    arr.push(randomChoice(range(1, 7)));
+  }
+  return arr;
 }
 
 function generate_iterations(level) {
@@ -78,23 +70,37 @@ function generate_time_rest(level) {
   return randomChoice(range(min, max));
 }
 
-function generate_combination(level) {
-  let min = 3;
-  let max = 5;
-  if (level === "Middle") {
-    min = 5;
-    max = 7;
+let COMBINATION_ARR_DEFAULT = generate_combination(level);
+let COMBINATION_STR_DEFAULT = COMBINATION_ARR_DEFAULT.join("");
+let TIME_REST_DEFAULT = generate_time_rest(level);
+let ITERATIONS_DEFAULT = generate_iterations(level);
+
+function fill_placeholders(
+  combination_str = COMBINATION_STR_DEFAULT,
+  time_rest = TIME_REST_DEFAULT,
+  iterations = ITERATIONS_DEFAULT
+) {
+  document.getElementById("combination").placeholder = combination_str;
+  document.getElementById("time_rest").placeholder = time_rest;
+  document.getElementById("iterations").placeholder = iterations;
+}
+
+function get_data() {
+  let combination_str = document.getElementById("combination").value;
+  let time_rest = document.getElementById("time_rest").value;
+  let iterations = document.getElementById("iterations").value;
+
+  // If user doesn't fill some input, use default constants
+  if (combination_str === "") {
+    combination_str = COMBINATION_STR_DEFAULT;
   }
-  if (level === "Profi") {
-    min = 7;
-    max = 10;
+  if (time_rest === "") {
+    time_rest = TIME_REST_DEFAULT;
   }
-  let comb_length = randomChoice(range(min, max));
-  let arr = [];
-  for (var i = 0; i < comb_length; i++) {
-    arr.push(randomChoice(range(1, 7)));
- }
-  return arr;
+  if (iterations === "") {
+    iterations = ITERATIONS_DEFAULT;
+  }
+  alert(combination_str + time_rest + iterations);
 }
 
 fill_placeholders();
